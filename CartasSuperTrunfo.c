2cc14b250eb3da863b57cef38d4c9b240fd18f2d
wpmedina <wmedinarox@gmail.com>
2025-09-19 12:30:34 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,77 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    // Carta 1
-    char estado1;
-    char codigo1[4];
-    char nomeCidade1[50];
-    int populacao1;
-    float area1, pib1;
-    int pontosTuristicos1;
+    printf("Carta 1:\n\n");
+    printf("Estado: A\n");
+    printf("Codigo: A01\n");
+    printf("Nome da Cidade: Sao Paulo\n");
+    printf("Populacao: 12325000\n");
+    printf("Area: 1521.11 km2\n");
+    printf("PIB: 699.28 bilhoes de reais\n");
+    printf("Numero de Pontos Turisticos: 50\n");
 
-    // Carta 2
-    char estado2;
-    char codigo2[4];
-    char nomeCidade2[50];
-    int populacao2;
-    float area2, pib2;
-    int pontosTuristicos2;
-
-    // Cadastro da primeira carta
-    printf("Cadastro da Carta 1:\n");
-    printf("Estado: ");
-    scanf(" %c", &estado1);
-    printf("Codigo: ");
-    scanf("%s", codigo1);
-    printf("Nome da Cidade: ");
-    scanf(" %[^\n]", nomeCidade1);
-    printf("Populacao: ");
-    scanf("%d", &populacao1);
-    printf("Area (km2): ");
-    scanf("%f", &area1);
-    printf("PIB (bilhoes de reais): ");
-    scanf("%f", &pib1);
-    printf("Numero de Pontos Turisticos: ");
-    scanf("%d", &pontosTuristicos1);
-
-    // Cadastro da segunda carta
-    printf("\nCadastro da Carta 2:\n");
-    printf("Estado: ");
-    scanf(" %c", &estado2);
-    printf("Codigo: ");
-    scanf("%s", codigo2);
-    printf("Nome da Cidade: ");
-    scanf(" %[^\n]", nomeCidade2);
-    printf("Populacao: ");
-    scanf("%d", &populacao2);
-    printf("Area (km2): ");
-    scanf("%f", &area2);
-    printf("PIB (bilhoes de reais): ");
-    scanf("%f", &pib2);
-    printf("Numero de Pontos Turisticos: ");
-    scanf("%d", &pontosTuristicos2);
-
-    // Exibindo os dados cadastrados
-    printf("\n--- Cartas Cadastradas ---\n");
-
-    printf("\nCarta 1:\n");
-    printf("Estado: %c\n", estado1);
-    printf("Codigo: %s\n", codigo1);
-    printf("Cidade: %s\n", nomeCidade1);
-    printf("Populacao: %d\n", populacao1);
-    printf("Area: %.2f km2\n", area1);
-    printf("PIB: %.2f bilhoes de reais\n", pib1);
-    printf("Pontos Turisticos: %d\n", pontosTuristicos1);
-
-    printf("\nCarta 2:\n");
-    printf("Estado: %c\n", estado2);
-    printf("Codigo: %s\n", codigo2);
-    printf("Cidade: %s\n", nomeCidade2);
-    printf("Populacao: %d\n", populacao2);
-    printf("Area: %.2f km2\n", area2);
-    printf("PIB: %.2f bilhoes de reais\n", pib2);
-    printf("Pontos Turisticos: %d\n", pontosTuristicos2);
+    printf("\nCarta 2:\n\n");
+    printf("Estado: B\n");
+    printf("Codigo: B02\n");
+    printf("Nome da Cidade: Rio de Janeiro\n");
+    printf("Populacao: 6748000\n");
+    printf("Area: 1200.25 km2\n");
+    printf("PIB: 300.50 bilhoes de reais\n");
+    printf("Numero de Pontos Turisticos: 30\n");
 
     return 0;
-
 }
